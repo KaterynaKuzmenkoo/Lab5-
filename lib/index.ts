4e4fef76f4ac31bc058a5a4a661e9d6fb5aae70c
KaterynaKuzmenkoo <131243749+KaterynaKuzmenkoo@users.noreply.github.com>
2024-04-18 18:54:03 +0200

    Add files via upload

--- lib/index.ts
+++ lib/index.ts
@@ -1,11 +1,9 @@
 import App from './app';
 import IndexController from "./controllers/index.controller";
-import DataController from './controllers/data.controller';
-
+import DataController from "./controllers/data.controller";
 const app: App = new App([
-   new DataController(),
-  // new IndexController()
-
-]);
-
+    //new IndexController(),
+    new DataController()
+ ]);
+ 
 app.listen();
